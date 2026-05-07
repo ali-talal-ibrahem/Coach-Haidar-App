d49d46390bab4300dbf58053c617d32a56ad4b9d
Ali Talal Ibrahem <aliibrahem760work@gmail.com>
2026-05-07 03:26:29 +0300

    Refactor draft saving and loading functions

--- script.js
+++ script.js
@@ -173,4 +173,10 @@ if (downloadBtn) {
 
         window.location.href = 'print.html';
     };
+
+    if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
 }
